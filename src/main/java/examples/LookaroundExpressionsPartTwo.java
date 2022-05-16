d7b0e805e025266e05c94beb6da4be8b293ae003
rostyslavmandybur <rmandybur@binariks.com>
2022-05-16 13:01:00 +0300

    refactored

--- src/main/java/examples/LookaroundExpressionsPartTwo.java
+++ src/main/java/examples/LookaroundExpressionsPartTwo.java
@@ -6,15 +6,18 @@ import java.util.regex.Pattern;
 public class LookaroundExpressionsPartTwo {
 
     public static void main(String[] args) {
+        System.out.println("captureAllPrices()");
         captureAllPrices("We have bought 5 javelins and every costs 130 000.00 $");
         captureAllPrices("ONLY today you can buy 3 lipsticks by price for 2. It just 14$ or 20,50 USD for 4 goods");
+
+        System.out.println("\n\nfindAllPhoneNumbersInSingleFormat()");
         findAllPhoneNumbersInSingleFormat("Hey Kile, here is my phones: +380687205039, 0964490664, 380 96 503 85 93 and 68 722 50 46");
     }
 
     public static void captureAllPrices(String text) {
         Pattern pattern = Pattern.compile("([\\d ]+[.,]?\\d{0,2})(?=[ ]?(?:\\$|USD))");
         Matcher matcher = pattern.matcher(text);
-        System.out.println(text + "\nWe found next price(s):");
+        System.out.println("TEXT: " + text + "\nWe found next price(s):");
         while (matcher.find()) {
             System.out.println(matcher.group(1));
         }
@@ -23,7 +26,7 @@ public class LookaroundExpressionsPartTwo {
     public static void findAllPhoneNumbersInSingleFormat(String text) {
         Pattern pattern = Pattern.compile("(?<=(?:[ +]380)|[0 ]|^)[ ]?((?:[ ]?\\d){9})(?=$|\\D{2})");
         Matcher matcher = pattern.matcher(text);
-        System.out.println(text + "\nWe found next phone number(s):");
+        System.out.println("TEXT: " + text + "\nWe found next phone number(s):");
         while (matcher.find()) {
             System.out.println(matcher.group(1).replaceAll("[ ]", ""));
         }
